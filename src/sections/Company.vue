7990a8ae894c104a932a9299cafeeea44339d1a1
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2018-02-08 14:07:17 +0100

    basic company mobile styling

--- src/sections/Company.vue
+++ src/sections/Company.vue
@@ -9,12 +9,15 @@
             <div class="text">
                 <h2>{{ headline }}</h2>
                 <p>{{ subheader }}</p>
+                <a :href="linkedIn.url" class="button">{{ linkedIn.text }}</a>
             </div>
         </div>
         <div class="awards">
             <h3>Awards</h3>
             <ul class="logos">
-                <li class="logo">Logo</li>
+                <li class="logo" v-for="logo in logos">
+                    <img :src="logo.logo.url" :alt="logo.logo.alt">
+                </li>
             </ul>
         </div>
     </section>
@@ -48,6 +51,19 @@
             jobTitle() {
                 if (!this.data) return;
                 return this.data['job-title'][0].text;
+            },
+
+            logos() {
+                if (!this.data) return;
+                return this.data.logos;
+            },
+
+            linkedIn() {
+                if (!this.data) return {};
+                return {
+                    url: this.data['linked-in'].url,
+                    text: this.data['linked-in-text'][0].text
+                }
             }
         }
     }
